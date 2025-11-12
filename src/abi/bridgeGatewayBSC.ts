c117b0146d236d4222615f6db22c46e02eacd053
Minh Hieu Tran <hieutm.site@gmail.com>
2025-11-12 08:42:17 +0700

    chore(abi): update bridge gateway bsc abi

--- src/abi/bridgeGatewayBSC.ts
+++ src/abi/bridgeGatewayBSC.ts
@@ -5,4 +5,7 @@ export const BRIDGE_GATEWAY_BSC_ABI = parseAbi([
   "function processed(uint256, bytes32) view returns (bool)",
   "function pause() external",
   "function unpause() external",
+
+  "event Locked(address indexed from, address indexed toOnLens, uint256 amount, uint256 nonce, uint256 dstChainId)",
+  "event Unlocked(address indexed toOnBsc, uint256 amount, uint256 amountOut, bytes32 indexed srcTxHash, uint256 srcChainId, uint256 srcNonce)",
 ]);
